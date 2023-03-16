1ee2a8c01cfb064586a2ac7df768cefb9ab59598
dengwenbin <dengwenbin@fengling.cn>
2023-03-16 18:29:29 +0800

    feat: 导出File

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './copy-option';
 export * from './file-factory';
 export * from './file-factory-base';
+export * from './file';
 export * from './i-directory';
 export * from './i-file';
 export * from './i-file-entry';
