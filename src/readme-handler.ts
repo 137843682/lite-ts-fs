e343f4e1a3e01b684ddc207f42d8d1f278131ca5
linhongjue <linhongjue@fengling.cn>
2023-03-20 19:37:58 +0800

    feat:新增更新微服务版本脚本

---
+++ src/readme-handler.ts
@@ -0,0 +1,23 @@
+import { HandlerBase } from './handler-base';
+
+import { IFile } from './i-file';
+
+const reg = /version-(\d+\.\d+\.\d+)-green/;
+
+export class ReadmeHandler extends HandlerBase {
+    public constructor(
+        private m_File: IFile,
+        version: string,
+    ) {
+        super(version);
+    }
+
+    public async handle() {
+        let text = await this.m_File.readString();
+        text = text.replace(reg, (text, match: string): string => {
+            const version = this.getVersion(match);
+            return text.replace(match, version);
+        });
+        await this.m_File.write(text);
+    }
+}
